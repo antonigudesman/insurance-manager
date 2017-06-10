efc418ddccd2887148b8dacb15525ecc6724b13b
Jason Cameron <jason@2doapp.com>
2017-06-10 14:03:36 +1000

    revise headings

--- bnchmrk/settings.py
+++ bnchmrk/settings.py
@@ -343,7 +343,7 @@ else:
     set_dynamic_settings(globals())
 
 EMPLOYER_THRESHOLD = 1
-EMPLOYER_THRESHOLD_MESSAGE = 'Current search criteria resulted in less 10 employers. Please expand it by selecting additional filters before refreshing.'
+EMPLOYER_THRESHOLD_MESSAGE = 'Too few employers in the comparison set. Please adjust the filters to capture more employers.'
 EMPLOYER_THRESHOLD_MESSAGE_ACCOUNT = 'Could not find employer name in account database'
 MIN_PERCENTILE = 2
 MAX_PERCENTILE = 100 - MIN_PERCENTILE
